21e78e1f86fc5842a1a4227aa0e6c76b41474af4
Rhys Kidd <rhyskidd@gmail.com>
2018-12-30 15:44:00 -0500

    autogen: add default patch prefix
    (Ported from radeon commit 8e6a4e96b7b27559e186f71b5547abb0a80b96dd)
    
    Signed-off-by: Rhys Kidd <rhyskidd@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -9,4 +9,7 @@ cd $srcdir
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
+git config --local --get format.subjectPrefix >/dev/null 2>&1 ||
+    git config --local format.subjectPrefix "PATCH xf86-video-ps3"
+
 $srcdir/configure --enable-maintainer-mode "$@"
